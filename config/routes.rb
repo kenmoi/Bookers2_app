ef649a99ac2125b90bb5dc2279d241c49c853a5a
kenmoi <ken.en001.mb@gmail.com>
2021-03-10 13:16:21 +0000

    [Start] 応用課題9

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'chats/show'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations'
-   }  
+   }
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "home/about" => "homes#about"
